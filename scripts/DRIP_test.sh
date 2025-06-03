5e939af4a2990e3b9204510d68549bc1b9180f0b
csienme <r13922142@csie.ntu.edu.tw>
2025-06-03 23:21:03 +0800

    TimeMixer++ modify

---
+++ scripts/DRIP_test.sh
@@ -0,0 +1,51 @@
+#export CUDA_VISIBLE_DEVICES=0
+
+model_name=TimeMixer
+
+seq_len=32
+pred_len=1
+e_layers=2
+down_sampling_layers=2
+down_sampling_window=2
+learning_rate=0.001
+d_model=16
+d_ff=16
+batch_size=32
+train_epochs=30
+patience=10
+
+
+
+python -u run.py \
+  --enable_DRIP 1 \
+  --num_stock  168 \
+  --comment no_clip\
+  --task_name stock_forecast_DRIP\
+  --is_training 0 \
+  --root_path ../NASDAQ_split_dataset \
+  --data_path ''\
+  --model_id 2011 \
+  --model $model_name \
+  --data MultipleStockDRIP\
+  --features MS \
+  --target close\
+  --seq_len $seq_len \
+  --label_len 0 \
+  --pred_len $pred_len \
+  --e_layers $e_layers \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 6 \
+  --dec_in 6 \
+  --c_out 6 \
+  --des 'Exp' \
+  --itr 1 \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --batch_size $batch_size \
+  --learning_rate $learning_rate \
+  --train_epochs $train_epochs \
+  --patience $patience \
+  --down_sampling_layers $down_sampling_layers \
+  --down_sampling_method avg \
+  --down_sampling_window $down_sampling_window
